f15385b3d1678e78bf62f913d99826d8dddaa9cb
Daan Kruijs <daan@227media.local>
2020-03-27 15:48:59 +0100

    Big refactoring of code, now only retrieves unread emails, added mark as unread functionality, updated function to persist to storage

--- src/data/get_gmails.py
+++ src/data/get_gmails.py
@@ -10,80 +10,163 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-
-# TODO borgen in README
-# combined steps from: 
-# https://developers.google.com/gmail/api/quickstart/python
-# https://codehandbook.org/how-to-read-email-from-gmail-api-using-python/
+from googleapiclient import errors
 
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+TOKEN_FILE = '../../token.pickle'
+CREDENTIALS_FILE = '../../credentials.json'
 
-def main(output_filepath):
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+class GMailGetter:
+    def __init__(self, credentials=None):
+        self.service = self.initialize_login(credentials)
+
+    def initialize_login(self, credentials):
+        """
+        Initializes a `service` object based on the user's credentials, requiring a manual 
+        log in and authorization via a web page for the first run. Loads cached token from a local file
+        on subsequent runs. Function requires a `credentials.json` file from a Google API export 
+        (https://developers.google.com/gmail/api/quickstart/python).
+
+        Parameters:
+            credentials (credentials object) -- optional
+
+        Returns:
+            GMail API `service` object.
+        """
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if credentials is not None:
+            service = build('gmail', 'v1', credentials=credentials)
+            return service    
+        else: 
+            # Reload previously stored credentials from pickle if possible;
+            if os.path.exists(TOKEN_FILE):
+                with open(TOKEN_FILE, 'rb') as token:
+                    credentials = pickle.load(token)
+
+            # If there are no (valid) credentials available, let the user log in manually (using a web page).
+            if not credentials or not credentials.valid:
+                if credentials and credentials.expired and credentials.refresh_token:
+                    credentials.refresh(Request())
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        CREDENTIALS_FILE, SCOPES) # TODO: modularize
+                    credentials = flow.run_local_server(port=0)
+                # Save the credentials for the next run
+                with open(TOKEN_FILE, 'wb') as token:
+                    pickle.dump(credentials, token)
+
+            service = build('gmail', 'v1', credentials=credentials)
+            return service
+
+    def get_unread_messages(self):
+        """
+        Retrieves all unread messages from the connected GMail account.
+
+        Returns:
+            A collection of GMail messages (actually threads).
+        """
+        service = self.service
+        results = service.users().messages().list(userId='me',labelIds = ['INBOX', 'UNREAD']).execute()
+        messages = results.get('messages', [])
+
+        if not messages:
+            print('No unread threads found.')
+            return None
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+            # print("Message snippets:")
+            # for message in messages:
+            #     msg = service.users().messages().get(userId='me', id=message['id']).execute()
+            #     print(msg['id'])
+            #     print(msg['internalDate'])
+            #     print(msg['payload']['headers'])
+            #     print(msg['snippet'])
+            #     try:
+            #         print(msg['payload']['body']['data'])
+            #     except KeyError:
+            #         print("no message body data found")
 
-    service = build('gmail', 'v1', credentials=creds)
+            return messages
 
-    # Call the Gmail API
-    # results = service.users().labels().list(userId='me').execute()
-    # labels = results.get('labels', [])
-    results = service.users().messages().list(userId='me',labelIds = ['INBOX']).execute()
-    messages = results.get('messages', [])
+    def mark_as_read(self, messages):
+        """
+        Marks each in a collection of messages (actually threads) as READ in the 
+        connected GMail account.
+        
+        Parameters:
+            messages: collection of GMail API messages objects.
 
-
-    if not messages:
-        print('No messages found.')
-    else:
-        print("Message snippets:")
-        for message in messages:
-            msg = service.users().messages().get(userId='me', id=message['id']).execute()
-            print(msg['id'])
-            print(msg['internalDate'])
-            print(msg['payload']['headers'])
-            print(msg['snippet'])
+        Returns:
+            processed_messages (collection of GMail API messages objects)
+        """
+        service = self.service
+        msg_labels = {'removeLabelIds': ['UNREAD'], 'addLabelIds': []}
+        processed_messages = []
+        for thread in messages:
             try:
-                print(msg['payload']['body']['data'])
-            except KeyError:
-                print("no message body data found")
+                thread = service.users().threads().modify(userId='me', id=thread['id'],
+                                              body=msg_labels).execute()
 
-            # save to disk
-            # json_object = json.loads(msg)
-            # TODO: msg['payload']['headers'][ITEREER if name=Received]['value'] om de afzender op te nemen in filename
-            with open(os.path.join(output_filepath, "_".join([msg['internalDate'], msg['id']])), "w") as file:
-                json.dump(msg, file)            
+                thread_id = thread['id']
+                label_ids = thread['messages'][0]['labelIds']
+
+                # TODO: convert to proper logging
+                print(f'Thread ID: {thread_id} - With Label IDs {label_ids}')
+                processed_messages.append(thread)
+
+            except errors.HttpError as error:
+                print(f'An error occurred: {error}')
+
+        return processed_messages
 
 
-# @click.command()
-# @click.argument('input_filepath', type=click.Path(exists=True))
-# @click.argument('output_filepath', type=click.Path())
-# def main(input_filepath, output_filepath):
-#     """ Runs data processing scripts to turn raw data from (../raw) into
-#         cleaned data ready to be analyzed (saved in ../processed).
-#     """
-#     logger = logging.getLogger(__name__)
-#     logger.info('making final data set from raw data')
+    def persist_to_storage(self, messages, local_path='../../data/raw', **gcp_metadata):
+        """
+        Persists a collection of messages to storage as JSON text files, optionally to GCP 
+        object storage or to a local path. 
+        
+        Parameters:
+            messages: collection of GMail API messages objects.
+            local_path (optional): a local path to save the files to.
+            **gcp_metadata: a dictionary object with required GCP metadata.
 
+        Returns:
+            file_names: a list of processed filenames or GCP object names.
+        """
+        service = self.service
+        if not gcp_metadata:
+            # we save to local storage
+            file_names = []
+            for message in messages:
+                # TODO: msg['payload']['headers'][ITEREER if name=Received]['value'] om de afzender op te nemen in filename
+                msg = service.users().messages().get(userId='me', id=message['id']).execute()
+                with open(os.path.join(local_path, "_".join([msg['internalDate'], msg['id']])) + '.json', "w") as file:
+                    json.dump(msg, file)
+                    file_names.append(file.name)
+
+            return file_names
+        else:
+            # we save to GCP storage
+            pass
+        
+
+def main():
+    # We instantiate a GMailGetter without credentials, instead using 
+    # the manual authentication and credentials caching logic.
+    mail_getter = GMailGetter()
+
+    unread_messages = mail_getter.get_unread_messages()
+    print('Found unread messages:', unread_messages, '\n')
+
+    if unread_messages is not None:
+        saved_to_disk = mail_getter.persist_to_storage(unread_messages)
+        print('Saved to disk:', saved_to_disk, '\n')
+
+        marked_as_read = mail_getter.mark_as_read(unread_messages)
+        print('Marked as read:', marked_as_read, '\n')
 
 
 if __name__ == '__main__':
@@ -97,4 +180,4 @@ if __name__ == '__main__':
     # load up the .env entries as environment variables
     # load_dotenv(find_dotenv())
 
-    main(os.path.join(project_dir, "data", "raw"))
+    main()
